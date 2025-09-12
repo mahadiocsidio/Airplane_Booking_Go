e75edecd36a7b00bcaf585114a118fa4d8f2f03b
mahadiocsidio <mahadiocsidio@students.amikom.ac.id>
2025-09-12 22:35:21 +0700

    minor add

---
+++ router/booking_route.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"airplane_booking_go/config"
+	"airplane_booking_go/controllers"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func BookRoutes(r *gin.Engine, client *mongo.Client, db string) {
+	bookingCollection := config.GetCollection(client, db, "booking")
+	flightCollection := config.GetCollection(client, db, "flights")
+	bookingController := controllers.NewBookingController(bookingCollection, flightCollection)
+
+	r.POST("/flights",bookingController.CreateBooking )
+	r.GET("/flights", bookingController.GetBookings)
+}
